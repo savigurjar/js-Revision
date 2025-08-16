35dc7dd38e674eb3deb75acb60632fded2a2c3be
savigurjar <savigurjar1201@gmail.com>
2025-08-16 19:01:45 +0530

    done

---
+++ Math in JS/mat.js
@@ -0,0 +1,18 @@
+// let num1 = 730;
+// let num2 = new Number(730);
+// let num3 = new Number(730);
+
+// console.log(num1 == num2); //true
+// console.log(num2 == num3); //false
+
+// console.log(num1 === num2); //false
+// console.log(num2 === num3); //false
+
+let nu = 271.36;
+// console.log(nu.toFixed(1)); //271.4
+// console.log(nu.toPrecision(2)) //2.7e+2
+
+// console.log(Math.floor(nu)); //271
+
+// 45 - 75 to rendom value
+console.log(Math.floor(Math.random() * (75 - 45 + 1) + 45));
