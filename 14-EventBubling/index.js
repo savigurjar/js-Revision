a54225e4219e73518e6549fff6f27e068b3b374b
savigurjar <savigurjar1201@gmail.com>
2025-09-04 16:28:09 +0530

    done practice

---
+++ 14-EventBubling/index.js
@@ -0,0 +1,22 @@
+const body = document.querySelector("body");
+
+// const buttons = document.querySelectorAll("button");
+
+// buttons.forEach((button) => {
+//     button.addEventListener("click", () => {   
+//         console.log(button.id)
+//         body.style.backgroundColor = button.id;
+//     });
+// });
+
+// optimize
+
+// const root = document.getElementById("root");
+
+// root.addEventListener("click", (event) => {
+
+//     if (event.target.tagName == 'BUTTON') {
+//         console.log(event.target.id)
+//         body.style.backgroundColor = event.target.id;
+//     }
+// })
